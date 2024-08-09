51be22a506fbcecaad5b7ce76e8df74734150dfb
Jerdssss <80202787+ErlJocson@users.noreply.github.com>
2024-08-09 17:12:10 +0800

    Update models.py

--- cards/models.py
+++ cards/models.py
@@ -6,3 +6,25 @@ class Cards(models.Model):
     video = models.FileField(upload_to='videos')
     link = models.CharField(max_length=500)
     position = models.IntegerField()
+
+# DynamoDB 
+# {
+# "ID": {
+# "N": "0"
+# },
+# "title": {
+# "S": ""
+# },
+# "logo" : {
+# "S":""
+# },
+# "video" : {
+# "S":""
+# },
+# "link" : {
+# "S":""
+# },
+# "position" : {
+# "N":"0"
+# }
+# }
