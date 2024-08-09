b05f5d0606651f09f09f164f080f7370ef61b3ff
erljocson <erl.jocson@teamhgs.com>
2024-08-09 17:39:53 +0800

    created a model using dynanorm: untested

--- cards/models.py
+++ cards/models.py
@@ -1,30 +1,9 @@
-from django.db import models
+from dynamorm import DynamoModel, fields
 
-class Cards(models.Model):
-    title = models.CharField(max_length=20)
-    logo = models.ImageField(upload_to='logos')
-    video = models.FileField(upload_to='videos')
-    link = models.CharField(max_length=500)
-    position = models.IntegerField()
-
-# DynamoDB 
-# {
-# "ID": {
-# "N": "0"
-# },
-# "title": {
-# "S": ""
-# },
-# "logo" : {
-# "S":""
-# },
-# "video" : {
-# "S":""
-# },
-# "link" : {
-# "S":""
-# },
-# "position" : {
-# "N":"0"
-# }
-# }
+class Cards(DynamoModel):
+    id = fields.String(hash_key=True)
+    title = fields.String()
+    logo = fields.String()
+    video = fields.String()
+    link = fields.String()
+    position = fields.Integer()
